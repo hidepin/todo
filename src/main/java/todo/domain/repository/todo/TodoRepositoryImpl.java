8ad350947eaba78b4ac2c8246557323841cee7b6
Hidetoshi Imai <hidepin@gmail.com>
2018-04-30 21:10:41 +0900

    Update TodoRepositoryImpl.

--- src/main/java/todo/domain/repository/todo/TodoRepositoryImpl.java
+++ src/main/java/todo/domain/repository/todo/TodoRepositoryImpl.java
@@ -1,45 +1,49 @@
 package todo.domain.repository.todo;
 
 import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import todo.domain.model.Todo;
 
 public class TodoRepositoryImpl implements TodoRepository {
 
+	private static final Map<String, Todo> TODO_MAP = new ConcurrentHashMap<String, Todo>();
+	
 	@Override
 	public Todo findOne(String todoId) {
-		// TODO Auto-generated method stub
-		return null;
+		return TODO_MAP.get(todoId);
 	}
 
 	@Override
 	public Collection<Todo> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return TODO_MAP.values();
 	}
 
 	@Override
 	public void create(Todo todo) {
-		// TODO Auto-generated method stub
-
+		TODO_MAP.put(todo.getTodoId(), todo);
 	}
 
 	@Override
 	public boolean update(Todo todo) {
-		// TODO Auto-generated method stub
-		return false;
+		TODO_MAP.put(todo.getTodoId(), todo);
+		return true;
 	}
 
 	@Override
 	public void delete(Todo todo) {
-		// TODO Auto-generated method stub
-
+		TODO_MAP.remove(todo.getTodoId());
 	}
 
 	@Override
 	public long countByFinished(boolean finished) {
-		// TODO Auto-generated method stub
-		return 0;
+		long count = 0;
+		for (Todo todo : TODO_MAP.values()) {
+			if (finished == todo.isFinished()) {
+				count++;
+			}
+		}
+		return count;
 	}
-
 }
