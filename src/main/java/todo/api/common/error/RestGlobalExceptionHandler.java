7f26be6c7fb3a97687cef819b229ed7da723be76
Hidetoshi Imai <hidepin@gmail.com>
2018-05-15 22:08:18 +0900

    Add Exception setting.

--- src/main/java/todo/api/common/error/RestGlobalExceptionHandler.java
+++ src/main/java/todo/api/common/error/RestGlobalExceptionHandler.java
@@ -3,9 +3,13 @@ package todo.api.common.error;
 import javax.inject.Inject;
 
 import org.springframework.context.MessageSource;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -30,5 +34,28 @@ public class RestGlobalExceptionHandler extends ResponseEntityExceptionHandler {
             Object... args) {
         return new ApiError(errorCode, messageSource.getMessage(errorCode,
                 args, request.getLocale()));
+    }
+    
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers,
+            HttpStatus status, WebRequest request) {
+        ApiError apiError = createApiError(request, "E400");
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            apiError.addDetail(createApiError(request, fieldError, fieldError
+                    .getField()));
+        }
+        for (ObjectError objectError : ex.getBindingResult().getGlobalErrors()) {
+            apiError.addDetail(createApiError(request, objectError, objectError
+                    .getObjectName()));
+        }
+        return handleExceptionInternal(ex, apiError, headers, status, request);
+    }
+
+    private ApiError createApiError(WebRequest request,
+            DefaultMessageSourceResolvable messageSourceResolvable,
+            String target) {
+        return new ApiError(messageSourceResolvable.getCode(), messageSource
+                .getMessage(messageSourceResolvable, request.getLocale()), target);
     }
 }
